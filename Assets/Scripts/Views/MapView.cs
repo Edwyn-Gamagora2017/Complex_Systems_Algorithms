0bc680071344bbf3b6eae8ef5639bcb3b90b0ddc
Edwyn Luis <edwynluis@yahoo.com.br>
2017-11-01 13:51:52 +0100

    adding a Map View

---
+++ Assets/Scripts/Views/MapView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapView : MonoBehaviour {
+
+	Map mapModel;			// stores the map information
+	[SerializeField]
+	GameObject mapContainer;
+	[SerializeField]
+	Sprite[] tileSprites;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
