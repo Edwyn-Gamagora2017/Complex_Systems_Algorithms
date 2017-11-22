ea91f6d08985cdf83d32de31c75b77a7db90f3ed
Unknown <edwynluis@yahoo.com.br>
2017-11-22 12:47:03 +0100

    Starting flocking

---
+++ Assets/Scripts/Flocking/BoidBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidBehaviour : MonoBehaviour {
+
+	[SerializeField]
+	FlockingCollider closeCollider;
+	[SerializeField]
+	FlockingCollider moveCollider;
+
+	/**
+	 * The boid will move close to the ones that are near
+	 * 		It obtains the average of positions
+	 */
+	void moveCloser(){
+		Vector3 average = new Vector3();
+
+		List<BoidBehaviour> boids = closeCollider.getCollidingBoids();
+		foreach( BoidBehaviour boid in boids ){
+			average += boid.transform.position - this.transform.position;
+		}
+		average /= boids.Count;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Flocking/FlockingCollider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingCollider : MonoBehaviour {
+
+	List<BoidBehaviour> collidingBoids ;
+
+	// Use this for initialization
+	void Start () {
+		this.collidingBoids = new List<BoidBehaviour>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public List<BoidBehaviour> getCollidingBoids(){
+		return this.collidingBoids;
+	}
+
+	// Some boid entered the zone
+	void OnTriggerEnter( Collider col ){
+		// Check if it a boid
+		BoidBehaviour boid = col.gameObject.GetComponent<BoidBehaviour>();
+		if( boid != null ){
+			collidingBoids.Add( boid );
+		}
+	}
+
+	// Some boid exited the zone
+	void OnTriggerExit( Collider col ){
+		// Check if it a boid
+		BoidBehaviour boid = col.gameObject.GetComponent<BoidBehaviour>();
+		if( boid != null ){
+			collidingBoids.Remove( boid );
+		}
+	}
+}
